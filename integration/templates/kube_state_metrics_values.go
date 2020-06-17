773bda12b8b7ca4c2a967b56d628097a21ef5dbe
Whites11 <whites11@gmail.com>
2020-06-17 09:54:51 +0200

    added installations's cidr to the allowed egress destinations in network policy

--- integration/templates/kube_state_metrics_values.go
+++ integration/templates/kube_state_metrics_values.go
@@ -3,4 +3,10 @@
 package templates
 
 // KubeStateMetricsValues values used for kube-state-metrics-app in integration test.
-const KubeStateMetricsValues = `e2e: true`
+const KubeStateMetricsValues = `e2e: true
+Installation:
+  V1:
+    Guest:
+      IPAM:
+        NetworkCIDR: "100.64.0.0/10"
+`
