e225fbc9e28b4e2493ae41c965338cb49eb70201
Marcel Mueller <marcel.mueller1@rwth-aachen.de>
2020-11-25 21:36:14 +0100

    Fix test

--- integration/test/basic/main_test.go
+++ integration/test/basic/main_test.go
@@ -64,7 +64,7 @@ func init() {
 
 	var latestRelease string
 	{
-		latestRelease, err = appcatalog.GetLatestVersion(ctx, catalogURL, appName)
+		latestRelease, err = appcatalog.GetLatestVersion(ctx, catalogURL, appName, "")
 		if err != nil {
 			panic(err.Error())
 		}
